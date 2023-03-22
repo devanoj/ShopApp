877541d48ac3fffaf543710a8240a75ad28bad64
Devano <C19312821@mytudublin.ie>
2023-03-22 21:43:15 +0000

    Commit 1 - "Stock Entity Created"

---
+++ app/src/main/java/com/example/shopapp/Entity/Stock.java
@@ -0,0 +1,73 @@
+package com.example.shopapp.Entity;
+
+import androidx.annotation.Nullable;
+
+public class Stock {
+    private String title;
+    private String manufacturer;
+    private double price;
+    private String category;
+    private String imageUrl;
+
+    public Stock() {
+        this.title="";
+        this.manufacturer="";
+        this.price=0;
+        this.category="";
+        this.imageUrl="";
+    }
+
+    public Stock(String title, String manufacturer, double price, String category, String imageUrl) {
+        this.title = title;
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.category = category;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
+
+}
---
+++ app/src/main/java/com/example/shopapp/Main/AddStock.java
@@ -0,0 +1,16 @@
+package com.example.shopapp.Main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shopapp.R;
+
+public class AddStock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_stock_layout);
+    }
+}
